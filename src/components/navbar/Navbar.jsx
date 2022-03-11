4bbe834a1571992bb88c637569083e2225bdbf95
karthikq <kk968346@gmail.com>
2022-03-11 16:50:37 +0530

    first2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,13 +12,27 @@ const Navbar = () => {
         className={
           navState ? "nav-contents nav-contents-active" : "nav-contents"
         }>
-        <div onClick={() => setNavState(!navState)}>
-          <label htmlFor="check" className="nav-icon">
-            <input type="checkbox" id="check" />
-            <span className="navspan"></span>
-            <span className="navspan"></span>
-            <span className="navspan"></span>
-          </label>
+        <label htmlFor="check" className="nav-icon">
+          <input
+            type="checkbox"
+            id="check"
+            onClick={() => setNavState(!navState)}
+          />
+          <span className="navspan"></span>
+          <span className="navspan"></span>
+          <span className="navspan"></span>
+        </label>
+        <div className="nav-items">
+          <div className="nav-profile">
+            <img src="" alt="" />
+            <span>Username</span>
+          </div>
+          <ul>
+            <li>Account</li>
+            <li>Favourites</li>
+            <li>Settings</li>
+            <li>login</li>
+          </ul>
         </div>
       </div>
     </div>
