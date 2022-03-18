ffba9a27cce3a816867c76f4c592cc55d8f61879
karthikq <kk968346@gmail.com>
2022-03-18 17:26:54 +0530

    bars8

--- src/pages/Options/OptionItem.jsx
+++ src/pages/Options/OptionItem.jsx
@@ -6,9 +6,9 @@ const OptionItem = ({ checkExists, selOptions, value, id }) => {
   return (
     <motion.div
       className="options-btndiv"
-      initial={{ opacity: 0, x: 50 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.8, delay: id / 6 }}
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, delay: id / 8 }}
       onClick={() => checkExists(value)}>
       <button
         type="button"
