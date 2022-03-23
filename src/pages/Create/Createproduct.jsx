3def6cf7bcb0cec387a9cf85fee6fb329d260b33
karthikq <kk968346@gmail.com>
2022-03-23 16:59:02 +0530

    newup2

--- src/pages/Create/Createproduct.jsx
+++ src/pages/Create/Createproduct.jsx
@@ -1,21 +1,51 @@
 /** @format */
 
 import React, { useState } from "react";
+import { BiRightArrowAlt } from "react-icons/bi";
 import "./createp.styles.scss";
 
 const Createproduct = () => {
   const [uploadedImg, setUploadedImg] = useState("");
+
+  const handleUploadedImg = (e) => {
+    setUploadedImg("");
+    const file = e.target.files[0];
+    const url = URL.createObjectURL(file);
+    setUploadedImg(url);
+  };
+  const onSubmit = (e) => {
+    e.preventDefult();
+  };
+
   return (
     <div className="create-p_container">
       <div className="create-p_contents">
         <h3>Upload Product</h3>
-        <div className="create-p_form">
-          <input type="text" placeholder="Name" />
-          <label htmlFor="upload_img">Select Image</label>
-          <input type="file" id="upload_img" className="upload_img" />
-          <img src="" alt="error" className="uploaded-img" />
-          <textarea name="p_desp" cols="30" rows="8"></textarea>
-        </div>
+        <form className="create-p_form">
+          <div className="create-p_form-contents">
+            <input type="text" placeholder="Name" />
+            <label htmlFor="upload_img">Select Image</label>
+            <input
+              type="file"
+              id="upload_img"
+              className="upload_img"
+              onChange={handleUploadedImg}
+            />
+            {uploadedImg && (
+              <div className="create-p_uploaded-img">
+                <img src={uploadedImg} alt="error" className="uploaded-img" />
+              </div>
+            )}
+            <textarea
+              name="p_desp"
+              cols="30"
+              rows="8"
+              placeholder="description(optional)"></textarea>
+          </div>
+          <button className="create-p_btn" onClick={onSubmit}>
+            <BiRightArrowAlt className="create-p_icon" />
+          </button>
+        </form>
       </div>
     </div>
   );
