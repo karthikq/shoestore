527bbd5c4e15e0f8bf541ff62085ba2f014c95bd
karthikq <kk968346@gmail.com>
2022-03-23 15:29:07 +0530

    create page

---
+++ src/pages/Create/Createproduct.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import "./createp.styles.scss";
+
+const Createproduct = () => {
+  return (
+    <div className="create-p_container">
+      <div className="create-p_contents"></div>
+    </div>
+  );
+};
+
+export default Createproduct;
